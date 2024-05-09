5a686f7538d17b34157abd1316e188d30cb533a8
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-05-09 13:17:43 -0700

    Add mock movie data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Movies from "./components/Movies";
+import movieData from "./moviesData";
 
 function App() {
+  const [moviesData, setMoviesData] = useState(movieData.movies);
+
   return (
     <div className="App">
       <Header />
