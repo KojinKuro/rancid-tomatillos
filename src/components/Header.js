a2afd8d8ae61b9301946bfff5c52080478fb79d4
Theo McCray <tlmccraycray@gmail.com>
2024-05-09 15:06:18 -0600

    Add header rendering logic inside Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,16 @@
 import "./Header.css";
+import React from "react";
 
 export default function Header() {
-  return <header>Header</header>;
+  return (
+  <header className="header">
+    <h1 className="logo">
+      RANCID <span className="green-logo"> T</span>O<span>MATILLOS</span>
+    </h1>
+    <div className="search-bar">
+      <input type="text" placeholder="Search movies" />
+      <button type="button" className="search-button">🔍</button>
+    </div>
+  </header>
+  );
 }
