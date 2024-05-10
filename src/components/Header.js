b327dea28b46a567600ab48e33a584dd74072b11
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-05-10 01:55:35 -0700

    fix: Clean up code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,12 @@
 import "./Header.css";
-import React, {useState} from "react";
+import Logo from "./Logo";
+import SearchBar from "./SearchBar";
 
 export default function Header({ movies }) {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filteredMovies, setFilteredMovies] = useState([]);
-
-  const handleSearchChange = (event) => {
-    const value = event.target.value;
-    setSearchTerm(value);
-    if (value.length > 0) {
-      const filtered = movies.filter(movie => 
-        movie.title.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredMovies(filtered);
-    } else {
-      setFilteredMovies([]);
-    }
-  };
-
   return (
-  <header className="header">
-    <h1 className="logo">
-      RANCID T<span className="green-logo">O</span>MATILLOS
-    </h1>
-    <div className="search-bar">
-      <input type="text" placeholder="Search movies" value={searchTerm} onChange={handleSearchChange}/>
-      <button type="button" className="search-button">🔍</button>
-      {filteredMovies.length > 0 && (
-        <ul className="search-results">
-          {filteredMovies.map(movie => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
-      )}  
-    </div>
-  </header>
+    <header className="header">
+      <Logo />
+      <SearchBar movies={movies} />
+    </header>
   );
 }
