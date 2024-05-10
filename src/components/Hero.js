66bafb5fa8128053d39502a2c686baa092964dde
Theo McCray <tlmccraycray@gmail.com>
2024-05-09 23:34:32 -0600

    Add css styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero({ movie }) {
       <div className="hero-content">
         <h1 className="hero-title">{title}</h1>
         <div className="hero-details">
-          <span className="hero-rating">{average_rating.toFixed(2)} / 5.0</span>
+          <span className="hero-rating">🟢 {average_rating.toFixed(1)} / 10.0</span>
           <span className="hero-year">{new Date(release_date).getFullYear()}</span>
           <p className="hero-overview">
             Some overview that is full of buzzwords to attempt to entice you to watch this movie and so on and so forth. We'll add a realtagline later.
