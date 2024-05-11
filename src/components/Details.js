039193242f7935dff23dde4292800cc41c6d3739
Theo McCray <tlmccraycray@gmail.com>
2024-05-10 20:10:25 -0600

    Add styling to Details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,23 +8,23 @@ export default function Details({ movie, onReturnHome }) {
       <button onClick={onReturnHome} className="return-home">Return Home</button>
       <div className="details-container">
         <img src={movie.backdrop_path} alt={`${movie.title} backdrop`} className="details-backdrop" />
+        <img src={movie.poster_path} alt={`${movie.title} poster`} className="details-poster" />
         <h1>{movie.title}</h1>
+        <div className="tagline"></div> Tagline: Movie info here soon 
+        <div className="movie-rating">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <Circle key={index} percentage={index < Math.round(movie.average_rating) ? 100 : 0} size="23px" />
+            ))}
+            <span>{movie.average_rating.toFixed(1)} / 10.0</span>
+        </div>
         <div className="movie-info">
-          <p>Tagline: Movie info here soon</p>
           <p>Release Date: {new Date(movie.release_date).getFullYear()}</p>
           <p>Length: 150min</p> {/* Placeholder */}
           <p>Genre: Drama</p> {/* Placeholder */}
           <p>Budget: $5000000</p> {/* Placeholder */}
           <p>Revenue: $550000</p> {/* Placeholder */}
-          <div className="movie-rating">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <Circle key={index} percentage={index < Math.round(movie.average_rating) ? 100 : 0} size="23px" />
-            ))}
-            <span>{movie.average_rating.toFixed(1)} / 10.0</span>
-          </div>
         </div>
       </div>
     </div>
   );
 }
-
