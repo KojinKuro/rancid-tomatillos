86d9795ce6eae6ad60e9659544de1d8a704a14e8
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-05-18 16:38:38 -0700

    Fix: update to use hash router

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import "boxicons";
 import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
